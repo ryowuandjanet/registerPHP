6eaaa6991fdbd911d6994210e714a50217caff39
ryowuandjanet <ryowu0329@gmail.com>
2021-11-29 18:10:12 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$page_title = "Home Page";
+include 'includes/header.php';
+include 'includes/navbar.php';
+?>
+
+<div class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h4>Login and Registration System in PHP</h4>
+        <h5>With Email Verification</h5>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'includes/footer.php';?>
+
